55ece93fd3a7270630787b8c6bef0f1345caa677
isongjosiah <35474477+isongjosiah@users.noreply.github.com>
2020-04-16 14:47:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -32,3 +32,41 @@ function yuGi(n) {
 }
 
 console.log(yuGi(10));
+
+newArr = []
+function yuGi2(n) {
+    for (i = 1; i <= n; i++) {
+        if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+            newArr.push("yu-gi-oh")
+
+        }
+        else if (i % 3 == 0 && i % 5 == 0) {
+            newArray.push("gi-oh")
+
+        }
+        else if (i % 2 == 0 && i % 5 == 0) {
+            newArr.push("yu-oh")
+        }
+        else if (i % 2 == 0 && i % 3 == 0) {
+            newArr.push("yu-gi")
+        }
+        else if (i % 5 == 0) {
+            newArr.push("oh")
+        }
+        else if (i % 3 == 0) {
+            newArr.push("gi")
+        }
+        else if (i % 2 == 0) {
+            newArr.push("yu")
+
+        } else {
+            newArr.push(i)
+        }
+    }
+
+    return newArr
+}
+
+console.log("")
+
+console.log(yuGi2(10))
